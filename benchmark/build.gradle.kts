7a302f1151351d57553648e8b397da11af9fdeba
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-04-29 13:10:56 +0200

    Introduce `basicTrackMethod` benchmark

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -55,4 +55,5 @@ dependencies {
     // in an app module this way - you will need to move any code you
     // want to benchmark to a library module:
     // https://developer.android.com/studio/projects/android-library#Convert
+    androidTestImplementation(project(":AutomatticTracks"))
 }
