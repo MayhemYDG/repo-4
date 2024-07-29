aa4568a0fb4c7d18d65487f0e410a3d5aec015c4
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-07-29 12:41:01 +0200

    Add possibility to call spans during performance transaction

--- crashlogging/src/main/java/com/automattic/android/tracks/crashlogging/performance/PerformanceTransactionRepository.kt
+++ crashlogging/src/main/java/com/automattic/android/tracks/crashlogging/performance/PerformanceTransactionRepository.kt
@@ -1,7 +1,12 @@
 package com.automattic.android.tracks.crashlogging.performance
 
+import android.util.Log
 import com.automattic.android.tracks.crashlogging.performance.internal.SentryPerformanceMonitoringWrapper
+import io.sentry.ISpan
 import io.sentry.ITransaction
+import io.sentry.NoOpSpan
+import io.sentry.SpanStatus
+import java.util.UUID
 
 /**
  * This class has to be a Singleton. It holds state of active performance transactions.
@@ -9,6 +14,7 @@ import io.sentry.ITransaction
 class PerformanceTransactionRepository internal constructor(private val sentryWrapper: SentryPerformanceMonitoringWrapper) {
 
     private val transactions: MutableMap<TransactionId, ITransaction> = mutableMapOf()
+    private val spans: MutableMap<SpanId, ISpan> = mutableMapOf()
 
     fun startTransaction(name: String, operation: TransactionOperation): TransactionId {
         return sentryWrapper.startTransaction(name, operation.value, true).let {
@@ -20,5 +26,34 @@ class PerformanceTransactionRepository internal constructor(private val sentryWr
 
     fun finishTransaction(transactionId: TransactionId, transactionStatus: TransactionStatus) {
         transactions[transactionId]?.finish(transactionStatus.toSentrySpanStatus())
+    }
+
+    fun startSpan(transactionId: TransactionId, spanName: String, operation: String): SpanId {
+        val transaction = transactions[transactionId]
+        val spanId = SpanId("$transactionId - ${UUID.randomUUID()}")
+
+        return if (transaction != null) {
+            val span = transaction.startChild(spanName, operation)
+            spans[spanId] = span
+            spanId
+        } else {
+            Log.e("PerformanceTransaction", "Transaction with id $transactionId not found")
+            NoOpSpan.getInstance()
+            SpanId("NoOpSpan")
+        }
+    }
+
+    /**
+     * @param spanHttpCode The HTTP status code of the span. This will be used to determine the span status, but doesn't mean that span is an HTTP span.
+     *
+     * @see io.sentry.SpanStatus
+     */
+    fun finishSpan(spanId: SpanId, spanHttpCode: Int, throwable: Throwable? = null) {
+        val span = spans[spanId]
+        if (span != null) {
+            throwable?.let { span.throwable = throwable }
+            span.status = SpanStatus.fromHttpStatusCode(spanHttpCode)
+        }
+        span?.finish()
     }
 }
