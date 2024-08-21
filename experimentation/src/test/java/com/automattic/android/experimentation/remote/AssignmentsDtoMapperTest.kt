41b49391b3ce1fd87b06f4d1c7629b4056ad40c5
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-08-21 16:11:53 +0200

    Add unit test to verify dto to domain mapping

---
+++ experimentation/src/test/java/com/automattic/android/experimentation/remote/AssignmentsDtoMapperTest.kt
@@ -0,0 +1,38 @@
+package com.automattic.android.experimentation.remote
+
+import com.automattic.android.experimentation.domain.Assignments
+import com.automattic.android.experimentation.domain.Variation
+import com.automattic.android.experimentation.domain.Variation.*
+import com.automattic.android.experimentation.remote.AssignmentsDtoMapper.toAssignments
+import org.junit.Assert.*
+import org.junit.Test
+
+class AssignmentsDtoMapperTest {
+
+    @Test
+    fun `mapping from dto to domain object is successful`() {
+        val ttl = 100
+        val fetchedAt = 123456L
+        val dto = AssignmentsDto(
+            variations = mapOf(
+                "experiment1" to "treatment1",
+                "experiment2" to "control",
+                "experiment3" to null
+            ),
+            ttl = ttl
+        )
+        val expected = Assignments(
+            variations = mapOf(
+                "experiment1" to Treatment("treatment1"),
+                "experiment2" to Control,
+                "experiment3" to Control
+            ),
+            ttl = ttl,
+            fetchedAt = fetchedAt
+        )
+
+        val result = dto.toAssignments(fetchedAt)
+
+        assertEquals(expected, result)
+    }
+}
