f4513966c9c3fbe88960cdeed1b59d7ae9f1750f
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-09-24 11:02:28 +0200

    Create/delete non-existing directory using setup/teardown methods
    This way, even if a test fails, the "non-existing" directory will be deleted

--- experimentation/src/test/java/com/automattic/android/experimentation/local/FileBasedCacheTest.kt
+++ experimentation/src/test/java/com/automattic/android/experimentation/local/FileBasedCacheTest.kt
@@ -7,14 +7,23 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
+import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
+import org.junit.Before
 import org.junit.Test
 import java.io.File
 import kotlin.io.path.createTempDirectory
 
 @OptIn(ExperimentalCoroutinesApi::class)
 internal class FileBasedCacheTest {
+
+    private lateinit var nonExistingCacheDir: File
+
+    @Before
+    fun setUp() {
+        nonExistingCacheDir = File("build/non-existent")
+    }
 
     @Test
     fun `saving and reading assignments is successful`() = runTest {
@@ -62,14 +71,18 @@ internal class FileBasedCacheTest {
 
     @Test
     fun `saving cache when cache dir doesnt exist is successful`() = runTest {
-        val cacheDir = File("build/non-existent").apply { assert(!this.exists()) }
-        val sut = fileBasedCache(this, cacheDir = cacheDir)
+        nonExistingCacheDir.apply { assert(!this.exists()) }
+        val sut = fileBasedCache(this, cacheDir = nonExistingCacheDir)
         sut.saveAssignments(TEST_ASSIGNMENTS)
 
         val result = sut.getAssignments()
 
         assertEquals(TEST_ASSIGNMENTS, result)
-        cacheDir.deleteRecursively()
+    }
+
+    @After
+    fun tearDown() {
+        nonExistingCacheDir.deleteRecursively()
     }
 
     private fun fileBasedCache(
